05a4f4a9732f480c46fa3cc16c83ae7d2af265be
chenhaikun <1029577428@qq.com>
2018-12-02 16:29:29 +0800

    aaaa

--- ThreadPool/src/com/chenhai/thread/pool/ThreadPool.java
+++ ThreadPool/src/com/chenhai/thread/pool/ThreadPool.java
@@ -21,10 +21,7 @@ public final class ThreadPool {
 		
 		ThreadPool.worker_num=worker_num;
 		workThreads=new WorkThread[worker_num];
-		for (int i = 0; i <worker_num; i++) {
-			workThreads[i]=new WorkThread();
-			workThreads[i].start();
-		}
+		
 	}
 	
 	public static ThreadPool getThreadPool() {
